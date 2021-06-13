897e375f2e773ba25d398e45de3cfd402a481490
Hart Traveller <hartktraveller@gmail.com>
2021-06-13 18:27:25 -0400

    new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'standard_scaler',
   packages = ['standard_scaler'],
-  version = '0.1',     
+  version = '0.2',     
   license='MIT',    
   description = 'An alternative to scikit-learn standard scaler', 
   author = 'Hart Massie-Keller',
